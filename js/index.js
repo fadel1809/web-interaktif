c712312ec7e0478208e91e7a0ffc5e8689363d20
Fadel <fadelmaulana12@gmail.com>
2023-12-22 20:32:53 +0700

    Section-1 Done

--- js/index.js
+++ js/index.js
@@ -19,3 +19,33 @@ function displayContent() {
   var header = document.querySelector(".header");
   header.classList.remove("preload");
 }
+
+var video = document.getElementById("myVideo");
+var button = document.getElementById("toggleButton");
+
+function toggleVideo() {
+  if (video.paused) {
+    video.play();
+    button.innerHTML = "<i class='fas fa-pause'></i>";
+    button.style.display = "none";
+  } else {
+    video.pause();
+    button.innerHTML = "<i class='fas fa-play'></i>";
+    button.style.display = "block";
+  }
+}
+
+video.addEventListener("ended", function () {
+  // Jika video selesai, reset tampilan tombol
+  button.innerHTML = "<i class='fas fa-play'></i>";
+  button.style.display = "block";
+});
+
+video.addEventListener("pause", function () {
+  button.innerHTML = "<i class='fas fa-pause'></i>";
+  button.style.display = "block";
+});
+
+video.addEventListener("click", function () {
+  toggleVideo();
+});
